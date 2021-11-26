06a6071590354a3b98cbc65a63cf277321607808
shs <test@mail.ru>
2021-11-27 01:39:54 +0300

    up to task 6

--- client.py
+++ client.py
@@ -1,16 +1,45 @@
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('192.168.2.227', 9090))
+DEFAULT_HOST = 'localhost'
+DEFAULT_PORT = 9090
 
-# msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
 
-data = sock.recv(1024)
+def host_or_default(host):
+    try:
+        lst = host.split('.')
+        if len(lst) != 4:
+            return DEFAULT_HOST
+        for i in range(4):
+            lst[i] = int(lst[i])
+            if lst[i] > 255 or lst[i] < 0:
+                return DEFAULT_HOST
+        return host
+    finally:
+        return DEFAULT_HOST
 
-sock.close()
 
-print(data.decode())
+def port_or_default(port):
+    try:
+        port = int(port)
+        return port if 1023 < port < 65536 else DEFAULT_PORT
+    finally:
+        return DEFAULT_PORT
+
+
+inp = input("Host: ")
+main_host = host_or_default(inp)
+
+inp = input("Port: ")
+main_port = port_or_default(inp)
+
+with socket.socket() as s:
+    s.connect((main_host, main_port))
+    print(f"Connected to: {(main_host, main_port)}")
+    while True:
+        msg = input("Message: ")
+        if msg == "exit":
+            exit()
+        s.send(msg.encode())
+        print(f"Data sent: {msg}")
+        data = s.recv(4096)
+        print(f"Server reply: {data.decode()}")
--- server.py
+++ server.py
@@ -1,28 +1,44 @@
 import socket
+import sys
+from datetime import datetime
 
 LOG_FILE_NAME = "log.txt"
-sock = socket.socket()
-sock.bind(('', 9090))
-
-log_file = open(LOG_FILE_NAME, "a")
+DEFAULT_PORT = 9090
 
 
-def log_print(text):
-    log_file.write(text + "\n")
+def port_or_default(text):
+    try:
+        res = int(text)
+        return res if 1023 < res < 65536 else DEFAULT_PORT
+    finally:
+        return DEFAULT_PORT
 
 
-while True:
-    sock.listen(0)
-    conn, addr = sock.accept()
-    log_print(F"Connected {addr}")
-    msg = ''
-    data = conn.recv(4096)
-    msg += data.decode()
-    if msg == "exit":
-        log_print("Received exit signal, closing")
-        break
-    log_print(F"Recieved {msg}")
-    conn.send(data)
+def logger(text):
+    with open(LOG_FILE_NAME, "a") as f:
+        f.write(f"{datetime.now()} {text}\n")
 
-log_file.close()
-conn.close()
+
+logger("Server started")
+args = sys.argv[1:]
+main_port = port_or_default(args[0]) if len(args) > 0 else DEFAULT_PORT
+
+with socket.socket() as s:
+    s.bind(('', main_port))
+    s.listen(5)
+    logger(f"Listening on port: {main_port}")
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            logger(F"Connected by {addr}")
+            while True:
+                data = conn.recv(4096)
+                msg = data.decode()
+                if not data:
+                    logger(F"Disconnected by {addr}")
+                    break
+                logger(F"Received {msg} from {addr}")
+                conn.send(data)
+                logger(F"Sent {msg} back to {addr}")
+
+logger(f"Server stopped")
